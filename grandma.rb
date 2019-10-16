d4a8949325cdf35480c45c7dffa2ad45c1e8e00a
Nicholas Ng <nicholasykng@gmail.com>
2019-10-16 03:43:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
 elsif phrase.downcase == phrase
